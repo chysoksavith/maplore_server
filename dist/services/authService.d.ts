68ecaabbcdd4bb6bdc81a51de4721ae7ae37cac4
chysoksavith <chysavith089@gmail.com>
2026-03-22 14:04:09 +0700

    gitignore update: remove dist directory from tracking

--- dist/services/authService.d.ts
+++
@@ -1,8 +0,0 @@
-import { registerSchema, loginSchema } from '../utils/validation';
-import { z } from 'zod';
-export declare const registerUser: (userData: z.infer<typeof registerSchema>) => Promise<any>;
-export declare const loginUser: (loginData: z.infer<typeof loginSchema>) => Promise<{
-    user: any;
-    token: string;
-}>;
-//# sourceMappingURL=authService.d.ts.map
